e714754c7d6f2c148e4a77c80fe6157adaffc010
ferriarnus <61201275+ferriarnus@users.noreply.github.com>
2023-08-28 18:45:05 +0200

    update to single block again

--- src/main/java/com/ferriarnus/adventurersdimension/block/AdventurersWorkbench.java
+++ src/main/java/com/ferriarnus/adventurersdimension/block/AdventurersWorkbench.java
@@ -39,11 +39,9 @@ import net.minecraftforge.network.NetworkHooks;
 import org.jetbrains.annotations.Nullable;
 
 public class AdventurersWorkbench extends Block implements EntityBlock {
-    public static final EnumProperty<DoubleBlockHalf> HALF = BlockStateProperties.DOUBLE_BLOCK_HALF;
 
     public AdventurersWorkbench(Properties pProperties) {
         super(pProperties);
-        this.registerDefaultState(this.stateDefinition.any().setValue(HALF, DoubleBlockHalf.LOWER));
     }
 
     @Override
@@ -51,9 +49,6 @@ public class AdventurersWorkbench extends Block implements EntityBlock {
         if (pLevel.isClientSide) {
             return InteractionResult.SUCCESS;
         } else {
-            if (pState.getValue(HALF) == DoubleBlockHalf.UPPER) {
-                pPos = pPos.below();
-            }
             BlockEntity blockentity = pLevel.getBlockEntity(pPos);
             if (blockentity instanceof AdventurersWorkbenchBlockEntity anchor) {
                 BlockPos finalPPos = pPos;
@@ -77,80 +72,15 @@ public class AdventurersWorkbench extends Block implements EntityBlock {
     @Nullable
     @Override
     public BlockEntity newBlockEntity(BlockPos pPos, BlockState pState) {
-        if (pState.getValue(HALF) == DoubleBlockHalf.UPPER) {
-            return null;
-        }
         return BlockEntityRegistry.ADVENTURERS_WORKBENCH.get().create(pPos, pState);
     }
 
     @Override
-    public BlockState updateShape(BlockState pState, Direction pFacing, BlockState pFacingState, LevelAccessor pLevel, BlockPos pCurrentPos, BlockPos pFacingPos) {
-        DoubleBlockHalf doubleblockhalf = pState.getValue(HALF);
-        if (pFacing.getAxis() != Direction.Axis.Y || doubleblockhalf == DoubleBlockHalf.LOWER != (pFacing == Direction.UP) || pFacingState.is(this) && pFacingState.getValue(HALF) != doubleblockhalf) {
-            return doubleblockhalf == DoubleBlockHalf.LOWER && pFacing == Direction.DOWN && !pState.canSurvive(pLevel, pCurrentPos) ? Blocks.AIR.defaultBlockState() : super.updateShape(pState, pFacing, pFacingState, pLevel, pCurrentPos, pFacingPos);
-        } else {
-            return Blocks.AIR.defaultBlockState();
-        }
-    }
-
-    @Override
-    public void setPlacedBy(Level pLevel, BlockPos pPos, BlockState pState, LivingEntity pPlacer, ItemStack pStack) {
-        BlockPos blockpos = pPos.above();
-        pLevel.setBlock(blockpos, this.defaultBlockState().setValue(HALF, DoubleBlockHalf.UPPER), 3);
-    }
-
-    @Override
-    public void playerWillDestroy(Level pLevel, BlockPos pPos, BlockState pState, Player pPlayer) {
-        if (!pLevel.isClientSide && pPlayer.isCreative()) {
-            preventCreativeDropFromBottomPart(pLevel, pPos, pState, pPlayer);
-        }
-        super.playerWillDestroy(pLevel, pPos, pState, pPlayer);
-    }
-
-    protected static void preventCreativeDropFromBottomPart(Level pLevel, BlockPos pPos, BlockState pState, Player pPlayer) {
-        DoubleBlockHalf doubleblockhalf = pState.getValue(HALF);
-        if (doubleblockhalf == DoubleBlockHalf.UPPER) {
-            BlockPos blockpos = pPos.below();
-            BlockState blockstate = pLevel.getBlockState(blockpos);
-            if (blockstate.is(pState.getBlock()) && blockstate.getValue(HALF) == DoubleBlockHalf.LOWER) {
-                BlockState blockstate1 = blockstate.getFluidState().is(Fluids.WATER) ? Blocks.WATER.defaultBlockState() : Blocks.AIR.defaultBlockState();
-                pLevel.setBlock(blockpos, blockstate1, 35);
-                pLevel.levelEvent(pPlayer, 2001, blockpos, Block.getId(blockstate));
-            }
-        }
-
-    }
-
-    @Override
-    public void playerDestroy(Level pLevel, Player pPlayer, BlockPos pPos, BlockState pState, @javax.annotation.Nullable BlockEntity pTe, ItemStack pStack) {
-        super.playerDestroy(pLevel, pPlayer, pPos, Blocks.AIR.defaultBlockState(), pTe, pStack);
-    }
-
-    @Override
-    public boolean canSurvive(BlockState pState, LevelReader pLevel, BlockPos pPos) {
-        BlockPos blockpos = pPos.below();
-        BlockState blockstate = pLevel.getBlockState(blockpos);
-        return pState.getValue(HALF) == DoubleBlockHalf.LOWER ? blockstate.isFaceSturdy(pLevel, blockpos, Direction.UP) : blockstate.is(this);
-    }
-
-    @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
-        pBuilder.add(HALF);
-    }
-
-    @Override
-    public long getSeed(BlockState pState, BlockPos pPos) {
-        return Mth.getSeed(pPos.getX(), pPos.below(pState.getValue(HALF) == DoubleBlockHalf.LOWER ? 0 : 1).getY(), pPos.getZ());
-    }
-
-    @Override
     public void onRemove(BlockState pState, Level pLevel, BlockPos pPos, BlockState pNewState, boolean pIsMoving) {
-        if (!pState.is(pNewState.getBlock()) && pState.getValue(HALF) == DoubleBlockHalf.LOWER) {
-            BlockEntity blockentity = pLevel.getBlockEntity(pPos);
-            if (blockentity instanceof AdventurersWorkbenchBlockEntity anchor) {
-                Containers.dropContents(pLevel, pPos, new RecipeWrapper(anchor.getHandler()));
-            }
-            super.onRemove(pState, pLevel, pPos, pNewState, pIsMoving);
+        BlockEntity blockentity = pLevel.getBlockEntity(pPos);
+        if (blockentity instanceof AdventurersWorkbenchBlockEntity anchor) {
+            Containers.dropContents(pLevel, pPos, new RecipeWrapper(anchor.getHandler()));
         }
+        super.onRemove(pState, pLevel, pPos, pNewState, pIsMoving);
     }
 }
